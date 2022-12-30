875377caed1bf7f714d2a058d93978c128c9335f
Kuba <jakeic@st.amu.edu.pl>
2022-12-30 17:12:47 +0100

    deploy script polish

--- gitea/deploy.sh
+++ gitea/deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+echo "Creating virtual enviroment ..."
+python3 -m venv hetz_gitea_venv 
+echo "Installing dependencies ..."
+pip install -q hcloud
 echo "Initiating gitea cloud deploy ..."
-python3 a.py $(cat token_file.txt) $(find ~ -name *.pub | tail -n 1 | xargs cat) "478874"
+chmod +x a.py
+python3 a.py $(cat ../token_file.txt) $(find ~ -name *.pub | tail -n 1 | xargs cat) "478874"
