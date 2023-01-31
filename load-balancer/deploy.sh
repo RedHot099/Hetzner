c487b58c1222f8fdaf21b669e73e2dc268f0f07b
Kuba <jakeic@st.amu.edu.pl>
2023-01-31 12:28:44 +0100

    deploy fix

--- load-balancer/deploy.sh
+++ load-balancer/deploy.sh
@@ -3,6 +3,6 @@ echo "Creating virtual enviroment ..."
 python3 -m venv hetz_gitea_venv 
 echo "Installing dependencies ..."
 pip install -q hcloud
-echo "Initiating gitea cloud deploy ..."
+echo "Initiating load balancer cloud deploy ..."
 chmod +x a.py
 python3 a.py $(cat ../token_file.txt) $(find ~ -name *.pub | tail -n 1 | xargs cat) "478874"
